d235ea50389d422120e529c8f62fa73ebc91d338
RajenderAvula <rajenderavula28@gmail.com>
2026-09-20 19:02:31 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,20 @@
+// ImageToTable/settings.gradle.kts
+
 rootProject.name = "ImageToTable"
 
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        mavenCentral()
         google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }

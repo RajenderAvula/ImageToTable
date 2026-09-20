dda2a5c593c99dbda57f9cb3699cf4fa13988427
RajenderAvula <rajenderavula28@gmail.com>
2026-09-20 18:33:17 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// ImageToTable/build.gradle.kts
 plugins {
     kotlin("multiplatform") version "2.0.20"
     id("org.jetbrains.compose") version "1.7.0"
@@ -19,27 +18,23 @@ kotlin {
     androidTarget()
 
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.material)
-                implementation(compose.ui)
-            }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
         }
-        val jvmMain by getting {
-            dependencies {
-                implementation(compose.desktop.currentOs)
-                implementation("net.sourceforge.tess4j:tess4j:5.11.0")
-                implementation("org.openpnp:opencv:4.9.0-0")
-            }
+
+        jvmMain.dependencies {
+            implementation(compose.desktop.currentOs)
+            implementation("net.sourceforge.tess4j:tess4j:5.11.0")
+            implementation("org.openpnp:opencv:4.9.0-0")
         }
-        val androidMain by getting {
-            dependencies {
-                implementation("androidx.activity:activity-compose:1.9.2")
-                implementation("androidx.appcompat:appcompat:1.7.0")
-                implementation("cz.adaptech.tesseract4android:tesseract4android:4.7.0")
-            }
+
+        androidMain.dependencies {
+            implementation("androidx.activity:activity-compose:1.9.2")
+            implementation("androidx.appcompat:appcompat:1.7.0")
+            implementation("cz.adaptech.tesseract4android:tesseract4android:4.7.0")
         }
     }
 }

dd00c6e53c209c9432c13a689be5e4e07d00ffff
RajenderAvula <rajenderavula28@gmail.com>
2026-09-26 21:57:04 +0530

    Update TableData.kt

--- src/androidMain/kotlin/com/example/imagetotable/model/TableData.kt
+++ src/androidMain/kotlin/com/example/imagetotable/model/TableData.kt
@@ -441,7 +441,7 @@ class TableData(
     }
 }
 
-/*object TableRepository {
+object TableRepository {
     val tables: SnapshotStateList<TableData> = mutableStateListOf()
 
     fun saveOrUpdate(table: TableData) {
@@ -456,4 +456,4 @@ class TableData(
     fun deleteTable(id: String) {
         tables.removeAll { it.id == id }
     }
-}*/
+}
